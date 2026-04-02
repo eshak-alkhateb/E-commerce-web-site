85ae4916047e46f7f88777a4bd30434cfb7b2ada
eshak almhtrm <ashak.alkateb@gmail.com>
2026-04-02 03:16:45 +0300

    Make add to cart button interactive

--- scripts/arnab.js
+++ scripts/arnab.js
@@ -46,7 +46,7 @@ products.forEach((product) => {
             Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
             </button>
         </div>
@@ -54,3 +54,32 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+        let matchingItem;
+document.querySelectorAll('.js-add-to-cart').forEach((button) =>{
+    button.addEventListener('click', () => {
+        const productId = button.dataset.productId;// special attribute in HTML to store data, we can access it using dataset in JavaScript
+
+
+        cart.forEach((item) =>{
+            if(item.productId === productId){
+                matchingItem = item;
+            }
+        });
+        if(matchingItem){
+            matchingItem.quantity += 1;
+        }
+        else{
+        cart.push({
+            productId: productId,
+            quantity: 1
+        });
+        }
+        console.log(cart);
+    });
+});
+/*
+    Main idea of JavaScript:
+    1. save the data
+    2. generate the HTML
+    3. Make it interactive
+*/
